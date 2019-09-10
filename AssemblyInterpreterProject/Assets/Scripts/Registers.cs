b853b3b99104e42308d83d0f10b37f7431cf4b9b
Thiago Henrique <thiagohrh@gmail.com>
2019-09-10 01:40:25 -0300

    Created a public registry system

---
+++ AssemblyInterpreterProject/Assets/Scripts/Registers.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+public class Registers
+{
+    public static Dictionary<string, int> registry = new Dictionary<string, int>();
+    public Registers()
+    {
+        registry["AC"] = 0;
+        registry["PC"] = 0;
+        registry["N"] = 0;
+        registry["Z"] = 0;
+    }
+}
--- AssemblyInterpreterProject/Assets/Scripts/StartSetup.cs
+++ AssemblyInterpreterProject/Assets/Scripts/StartSetup.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class StartSetup : MonoBehaviour
 {
+    Registers registers = new Registers();
     void Start()
     {
         TextLoader instance = new TextLoader();
         string allWords = instance.ReadFromFile("example");
 
-
+        Debug.Log("AC: " + Registers.registry["AC"]);
     }
 }
