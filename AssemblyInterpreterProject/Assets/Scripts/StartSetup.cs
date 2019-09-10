36f58b6af9bce1929fcd959705adb0f5c6c8258e
Thiago Henrique <thiagohrh@gmail.com>
2019-09-10 00:56:39 -0300

    Added simple txt reading functionality

---
+++ AssemblyInterpreterProject/Assets/Scripts/StartSetup.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartSetup : MonoBehaviour
+{
+    void Start()
+    {
+        TextLoader instance = new TextLoader();
+        string allWords = instance.ReadFromFile("example");
+
+
+    }
+}
---
+++ AssemblyInterpreterProject/Assets/Scripts/TextLoader.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+using UnityEngine;
+
+public class TextLoader
+{
+    string folderPath = Application.dataPath + "/Resources/";
+    string fileType = ".txt";
+    public string ReadFromFile(string fileName)
+    {
+        string text = File.ReadAllText(folderPath + fileName + fileType);
+        return text;
+    }
+}
